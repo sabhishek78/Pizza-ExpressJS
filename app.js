f27231f2cf0ace18811889d46acc8255c0ae4c6f
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2026-08-29 17:30:20 +0530

    done

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// Middleware that allows Express to understand JSON request bodies
+app.use(express.json());
+
+
+// PIZZA DATA
 const pizzas = {
     margherita: {
         price: 200,
@@ -19,35 +24,189 @@ const pizzas = {
     }
 };
 
-app.use((req, res, next) => {
-    console.log(`${req.method} request received`);
-    next();
-});
 
-app.use((req, res, next) => {
-    console.log('Middleware 1');
-    next();
-});
+// CHECK PIZZA MIDDLEWARE
+const checkPizza = (req, res, next) => {
 
-app.use((req, res, next) => {
-    console.log('Middleware 2');
-    next();
-});
-
-app.get('/pizzas', (req, res) => {
-    res.send(pizzas);
-});
-
-
-app.get('/pizzas/:name', (req, res) => {
-  
     const pizzaName = req.params.name;
 
     if (!pizzas[pizzaName]) {
         return res.status(404).send('Pizza not found');
     }
 
-    res.send(pizzas[pizzaName]);
+    // Attach the pizza to req
+    req.pizza = pizzas[pizzaName];
+
+    next();
+};
+
+
+// LOGGING MIDDLEWARE
+app.use((req, res, next) => {
+
+    console.log(`${req.method} request received`);
+
+    next();
+});
+
+
+// =================================
+// GET ALL PIZZAS
+// GET /pizzas
+// =================================
+
+app.get('/pizzas', (req, res) => {
+
+    res.send(pizzas);
+
+});
+
+
+// =================================
+// GET ONE PIZZA
+// GET /pizzas/:name
+// =================================
+
+app.get('/pizzas/:name', checkPizza, (req, res) => {
+
+    res.send(req.pizza);
+
+});
+
+
+// =================================
+// CREATE A NEW PIZZA
+// POST /pizzas
+//
+// Example body:
+// {
+//     "name": "farmhouse",
+//     "price": 300,
+//     "quantity": 4
+// }
+// =================================
+
+app.post('/pizzas', (req, res) => {
+
+    const newPizza = req.body;
+
+    pizzas[newPizza.name] = {
+        price: newPizza.price,
+        quantity: newPizza.quantity
+    };
+
+    res.send(pizzas[newPizza.name]);
+
+});
+
+
+// =================================
+// ADD STOCK
+// POST /pizzas/:name/add
+//
+// Example:
+// POST /pizzas/paneer/add
+//
+// Body:
+// {
+//     "quantity": 5
+// }
+// =================================
+
+app.post('/pizzas/:name/add', checkPizza, (req, res) => {
+
+    req.pizza.quantity += req.body.quantity;
+
+    res.send(req.pizza);
+
+});
+
+
+// =================================
+// REMOVE STOCK
+// POST /pizzas/:name/remove
+//
+// Example:
+// POST /pizzas/paneer/remove
+//
+// Body:
+// {
+//     "quantity": 5
+// }
+// =================================
+
+app.post('/pizzas/:name/remove', checkPizza, (req, res, next) => {
+
+    if (req.pizza.quantity < req.body.quantity) {
+
+        const error = new Error('Not enough pizza in stock');
+
+        error.status = 400;
+
+        return next(error);
+    }
+
+    req.pizza.quantity -= req.body.quantity;
+
+    res.send(req.pizza);
+
+});
+
+
+// =================================
+// DELETE A PIZZA
+// DELETE /pizzas/:name
+// =================================
+
+app.delete('/pizzas/:name', checkPizza, (req, res) => {
+
+    const pizzaName = req.params.name;
+
+    delete pizzas[pizzaName];
+
+    res.status(204).send();
+
+});
+
+
+// =================================
+// RENAME A PIZZA
+// PUT /pizzas/:name/name
+//
+// Example:
+// PUT /pizzas/paneer/name
+//
+// Body:
+// {
+//     "newName": "cheese-paneer"
+// }
+// =================================
+
+app.put('/pizzas/:name/name', checkPizza, (req, res) => {
+
+    const oldName = req.params.name;
+
+    const newName = req.body.newName;
+
+    pizzas[newName] = pizzas[oldName];
+
+    delete pizzas[oldName];
+
+    res.send(pizzas[newName]);
+
+});
+
+
+// =================================
+// ERROR-HANDLING MIDDLEWARE
+// =================================
+
+app.use((err, req, res, next) => {
+
+    const status = err.status || 500;
+
+    res.status(status).send(err.message);
+
 });
 
 
